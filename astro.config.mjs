b138399afda2cd425401d2a4e0ef9c565108cc7b
grenly-del <ardiand400@gmail.com>
2026-02-14 17:56:02 +0800

    perbaiki masalah ssr di page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 
-  site: 'https://grantly.klabatdev.my.id',
-
   output: 'server',
 
   adapter: node({
+
     mode: 'standalone'
+
   }),
 
-  integrations: [
-    sitemap()
-  ],
-
   vite: {
+
     plugins: [tailwindcss()],
+
     ssr: {
+
       noExternal: ['gsap'],
+
     },
-  }
+
+  },
+
+  site: 'https://domainanda.com',
+
+  integrations: [sitemap()],
 
 });

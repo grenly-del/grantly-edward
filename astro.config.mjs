f44ad91261e48554ab2e4913a686c47b4154a214
grenly-del <ardiand400@gmail.com>
2026-02-14 17:33:25 +0800

    perbaiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,27 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
-import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
-// https://astro.build/config
+
 export default defineConfig({
+
+  site: 'https://grantly.klabatdev.my.id',
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [
+    sitemap()
+  ],
+
   vite: {
     plugins: [tailwindcss()],
     ssr: {
       noExternal: ['gsap'],
     },
-  },
+  }
 
-  site: 'https://example.com',
-  adapter: vercel(),
-  integrations: [sitemap()],
 });
